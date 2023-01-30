8f2e633e2c25d5dc0bc8996854d8b31a970cfce8
Ericlys Moreira <ericlysm@gmail.com>
2023-01-29 21:49:02 -0300

    setup server node

---
+++ streams/stream-http-server.js
@@ -0,0 +1,36 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class InverseNumberStream extends Transform { 
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    console.log(transformed)
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+//req -> ReadableStream
+//res -> WritableStream
+const server = http.createServer( async (req,res) => {
+  const buffer = []
+
+  //async/await
+
+  for await(const chunk of req) {
+    buffer.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffer).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
+
+  // return req
+  // .pipe(new InverseNumberStream)
+  // .pipe(res)
+})
+
+server.listen(3334)
